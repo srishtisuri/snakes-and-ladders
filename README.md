3f8730a7e064d41445568551ca4485b1766054f3
Srishti Suri <>
2023-10-07 18:34:09 +1100

    refactor: large scale code cleanup

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Snakes & Ladders
+
 Take a trip down memory lane with a game of Snakes &amp; Ladders. Built with Vanilla `HTML`, `JavaScript`, and `CSS` with no external NPM dependencies.
 
 srishtisuri.github.io/snakes-and-ladders/
@@ -7,32 +8,24 @@ srishtisuri.github.io/snakes-and-ladders/
 
 1. Each roll of the dice results in the piece being moved X number of squares
 2. If the piece lands on a snake (squares that are multiples of 9), it is moved back 3 squares
-3. If the piece lands on a ladder (square 25 or 55), it is moved ahead 10 squares 
+3. If the piece lands on a ladder (square 25 or 55), it is moved ahead 10 squares
 4. Towards the end of the game, the piece will not move unless the dice roll keeps it within the maximum number of squares
-For example: if it is at square 98 and you roll a 5, the piece will not move
+   For example: if it is at square 98 and you roll a 5, the piece will not move
 5. The game is won when the piece reaches the last square on the board
 
 ## How To Play
 
 - Open up your browser DevTools and navigate to the 'Console' tab
+- Type in `play()` and hit enter
 - While the game is running, the moves will be logged onto the console
-- Click the 'Play' button to start the game
-- Once 'Play' is clicked, it will be disabled to prevent a concurrent game from being triggered
-- You can click the 'Reset' button at any time. This will stop the game if it is running and will reset the game variables to their initial state
 
 ## Technical Notes
 
 Snakes and Ladders was a vanilla `JS` challenge. It was timeboxed and built with some key considerations in mind, as best as time would permit.
 
-### Scalability
-The game variables that are initialised at the beginning of index.js have been intentionally abstracted out of the core methods. In a future state, these abstractions would be tested for alternate scenarios. For example, `finishPosition` is leveraged by `totalSquares` which in turn drives how big the board is.
-
-### Readability
-There were many sections of the code where shorthand and clever syntax could have been used. For example, some of the `if` statements could have been ternary operators or switch statements. However, this was forgone in favour of more intuitive and easily legible code.
-
 ### User Interface
-The UI was not part of the original requirements but was built as a personal challenge. As certain controls were introduced to the UI, it created a need for some of the existing methods to be extended and for new code to be introduced. There are still ways in which the UI could be enhanced for a better user experience - this will be explored later.
 
+The UI was not part of the original requirements but was built as a personal challenge. As certain controls were introduced to the UI, it created a need for some of the existing methods to be extended and for new code to be introduced. There are still ways in which the UI could be enhanced for a better user experience - this will be explored later.
 
 ## Future Improvement Ideas
 
@@ -40,4 +33,3 @@ The UI was not part of the original requirements but was built as a personal cha
 - Show the game piece move to the snake and ladder pieces before redirecting
 - Introduce a pause button
 - Print the console logs somewhere in the UI
-- Prevent square numbers from disappearing when the piece is on it
--- index.js
+++ index.js
@@ -1,181 +1,74 @@
-/* initialise global variables */
-let game,
-    gamePiece = '🟣',
-    startingPosition = 1,
-    finishPosition = 100,
-    totalSquares = [...Array(finishPosition).keys()].map(x => x+1),
-    snakes = [...totalSquares.filter(x => x % 9 === 0)],
-    ladders = [25, 55],
-    currentPosition = startingPosition,
-    tempPosition;
+const checkSnake = (pos) => pos % 9 === 0;
+const checkLadder = (pos) => [25, 55].includes(pos);
+const checkOverflow = (pos) => pos > 100;
+const placePiece = (pos) =>
+  document.getElementById(`cell-${pos}`).classList.add("active");
+const removePiece = (pos) => {
+  document.getElementById(`cell-${pos}`).classList.remove("active") || null;
+};
 
-/* dice roll function that returns a promise based on which the turn is completed*/
 const rollDice = () => {
-    return new Promise((resolve, reject) => {
-        const roll = Math.floor(Math.random() * 6 + 1);
-        console.log(`You rolled a ${roll}`);
-
-        tempPosition = currentPosition + roll;
-        if (tempPosition < finishPosition || tempPosition === finishPosition) resolve(roll);
-        else reject(roll);
-    });
+  return new Promise((res) => {
+    res(Math.floor(Math.random() * 6) + 1);
+  });
 };
 
-/*
-    handles all scenarios for a valid dice roll 
-    1 - land on a snake (move back 3 spaces)
-    2 - land on a ladder (move up 10 spaces)
-    3 - land on the last square of the board (win the game) 
-*/ 
-const handleValidRoll = (roll) => {
-    currentPosition += roll;
-    if (tempPosition < finishPosition) {
-        console.log(`You are now at square ${currentPosition}`);
-
-        if (snakes.includes(tempPosition)) {
-            console.log('🐍 Oh no! There is a snake!');
-            currentPosition -= 3;
-            console.log(`You have been sent back to square ${currentPosition}`);
-        } else if (ladders.includes(tempPosition)) {
-            console.log('🪜 Jackpot! You see a ladder!');
-            currentPosition += 10;
-            console.log(`You have been sent ahead to square ${currentPosition}`);
-        }
-    } else {
-        console.log(`Congratulations, you won the game!`);
-        clearInterval(game);
-    }
-    console.log('------------------------------------');
+const calculateNewPosition = (tempPos) => {
+  if (checkSnake(tempPos)) return tempPos - 3;
+  else if (checkLadder(tempPos)) return tempPos + 10;
+  else return tempPos;
 };
 
-/* 
-    handles all scenarios for an invalid dice roll 
-    1 - your piece can only move up till the last square
-*/ 
-const handleInvalidRoll = (roll) => {
-    if (tempPosition > finishPosition) {
-        console.log(`You cannot move ahead`);
-        console.log(`You are still at square ${currentPosition}`);
-    };
-    console.log('------------------------------------');
+const generateMsg = (tempPos) => {
+  const isSnake = checkSnake(tempPos);
+  const isLadder = checkLadder(tempPos);
+  return isSnake ? `snake` : isLadder ? `ladder` : ``;
 };
 
-/* moves the game piece to its new position */
-const movePiece = () => {
-    const currentSpot = document.getElementsByClassName('active');
-    if (currentSpot.length > 0) { 
-        currentSpot[0].innerHTML = currentSpot[0].getAttribute('id').split('-')[1]; 
-        currentSpot[0].classList.remove('active');
-    };
+const play = async () => {
+  let currPos = 1;
+  console.log(`${currPos}`);
+  const game = setInterval(async () => {
+    removePiece(currPos);
+    const roll = await rollDice();
+    const tempPos = currPos + roll;
+    const isValid = !checkOverflow(tempPos);
+    const newPos = isValid ? calculateNewPosition(tempPos) : currPos;
+    const text = generateMsg(tempPos);
 
-    const newSpot = document.getElementById(`cell-${currentPosition}`);
-    newSpot.classList.add('active');
-    newSpot.innerHTML = gamePiece;
+    currPos = newPos;
+    console.log(`${roll}-${text}${currPos}`);
+
+    placePiece(currPos);
+    if (currPos === 100) clearInterval(game);
+  }, 1000);
 };
 
-/* main function */
-const play = () => {
-    document.getElementById('play').setAttribute('disabled', '');
-
-    console.log(`Welcome to Snakes & Ladders!`)
-    console.log(`Your starting position is ${currentPosition}`)
-    console.log('------------------------------------');
-
-    game = setInterval(() => {
-        rollDice()
-        .then((roll) => {
-            handleValidRoll(roll);
-            movePiece();
-        })
-        .catch((roll) => handleInvalidRoll(roll));
-    }, 1000);
-};
-
-/* drives the functionality of the reset button so that user can reinitiate */
-const reset = () => {
-    clearInterval(game);
-    currentPosition = startingPosition;
-    movePiece();
-    document.getElementById('play').removeAttribute('disabled');
-};
-
-/* main render function to paint dom */
 const render = () => {
-    /* heading */
-    const heading = document.createElement('h1');
-    heading.innerHTML = 'Snakes & Ladders';
-    document.body.appendChild(heading);
+  const table = document.createElement("table");
+  let id = 100;
+  let tr;
 
-    /* table */
-    const board = document.createElement('table');
-    board.setAttribute('id', 'board');
-    document.body.appendChild(board);
+  for (let i = 1; i < 11; i++) {
+    tr = document.createElement("tr");
+    tr.id = `row-${11 - i}`;
+    let row = [];
+    for (let j = 1; j < 11; j++) {
+      td = document.createElement("td");
+      td.id = `cell-${id}`;
+      td.innerHTML = id;
 
-    /* rows */
-    const rows = [...Array(10).keys()]
-    .map((x) => x+1)
-    .map((x) => {
-        const row = document.createElement('tr');
-        row.setAttribute('id', `row-${x}`);
-        return row;
-    }).reverse();
+      checkSnake(id) && (td.className = "snake");
+      checkLadder(id) && (td.className = "ladder");
 
-    /* cells */
-    const cells = [...totalSquares]
-    .map((x) => {
-        const cell = document.createElement('td');
-        cell.setAttribute('id', `cell-${x}`);
-        cell.innerHTML = x;
-        return cell;
-    });
-
-    let sortedCells = [];
-    for (let i = 10; i > 0; i--) {
-        const index = i*10;
-        const poppedItems = cells.slice(index - 10, index);
-        if (i % 2 === 0) {
-            poppedItems.reverse();
-        }
-        sortedCells = [...sortedCells, poppedItems];
-    };
-
-    /* add cells to rows */
-    for (let i in rows) {
-        for (let i in sortedCells) {
-            rows[i].append(...sortedCells[i]);
-        }
-        board.append(rows[i]);
-    };
-
-    /* add snakes and ladders */
-    for (let i of snakes) {
-        const snakeCell = document.getElementById(`cell-${i}`);
-        snakeCell.setAttribute('class', 'snake');
-    };
-
-    for (let i of ladders) {
-        const ladderCell = document.getElementById(`cell-${i}`);
-        ladderCell.setAttribute('class', 'ladder');
-    };
-
-    /* add game piece to starting position */
-    const startingSquare = document.getElementById(`cell-${currentPosition}`);
-    startingSquare.setAttribute('class', 'active');
-    startingSquare.innerHTML = gamePiece;
-
-    /* play button */
-    const playBtn = document.createElement('button');
-    playBtn.setAttribute('onclick', 'play()');
-    playBtn.setAttribute('id', 'play');
-    playBtn.innerHTML = 'Play';
-    document.body.append(playBtn);
-
-    /* reset button */
-    const exitBtn = document.createElement('button');
-    exitBtn.setAttribute('onclick', 'reset()');
-    exitBtn.setAttribute('id', 'reset');
-    exitBtn.innerHTML = 'Reset';
-    document.body.append(exitBtn);
+      row.push(td);
+      id--;
+    }
+    i % 2 === 0 ? tr.append(...row.reverse()) : tr.append(...row);
+    table.append(tr);
+  }
+  document.body.append(table);
+  placePiece(1);
 };
 
 render();
